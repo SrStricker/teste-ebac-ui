85f14639be9840ddade2ac293034d6ffd8d2c904
Luciano <lucianostrickercoelho.lsc@gmail.com>
2023-04-27 11:58:54 -0300

    nova modificação

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,17 @@
 /// <reference types="cypress" />
 
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+});
+
+afterEach(() => {
+    cy.screenshot()
+});
+
 context('funcionalidade Login', () =>{
 
     it('Deve fazer login com sucesso', () => {
 
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -15,7 +22,6 @@ context('funcionalidade Login', () =>{
 
     it('Deve exibir mensagem de erro ao inserir usuario invalido', () => {
 
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_junior@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -26,7 +32,6 @@ context('funcionalidade Login', () =>{
 
     it('Deve exibir mensagem de erro ao inserir senha invalida', () => {
 
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste123teste.com')
         cy.get('.woocommerce-form > .button').click()
