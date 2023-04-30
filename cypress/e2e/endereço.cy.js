c6cb9bff8c46b9d3036a8685ed1a06b953ee5df3
Luciano <lucianostrickercoelho.lsc@gmail.com>
2023-04-30 11:34:53 -0300

    nova modificação

--- cypress/e2e/endereço.cy.js
+++ cypress/e2e/endereço.cy.js
@@ -1,12 +1,17 @@
 /// <reference types= "cypress" />
+import EnderecoPage from "../support/page-objects/endereço.page"
 
 describe('Funcionalidade endereços - faturamento e entrega', () => {
-beforeEach(() => {
-    cy.visit('minha-conta')
-    cy.login('aluno_ebac@teste.com', 'teste@teste.com')
-}); 
+    beforeEach(() => {
+        cy.visit('minha-conta')
+        cy.fixture('perfil').then(dados => {
+            cy.login(dados.usuario, dados.senha)
+        })
 
-    it('deve fazer cadastro de faturamento com sucesso', () => {
-        
     });
-});
+
+    it.only('deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Melissa', 'Ferreira', 'Inmetrics', 'Brasil', 'Faisão', '545', 'Curitiba', 'Paraná', '83824420', '40028522', 'ferreiramel@ebac.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+}); 
